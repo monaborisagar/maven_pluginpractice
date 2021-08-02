05c2941f3826f8e0c6cf8a87b9eae87b3f386351
monaborisagar <borisagarmona107@gmail.com>
2021-08-02 17:28:07 -0400

    compiler jar added

---
+++ src/main/java/maven_pluginpractice/test.java
@@ -0,0 +1,10 @@
+package maven_pluginpractice;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("thuiis is");
+	}
+
+}
