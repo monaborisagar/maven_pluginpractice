7e81adda328b8d041f2e16e009e5b7e82bcd1311
monaborisagar <borisagarmona107@gmail.com>
2021-08-02 20:14:32 -0400

    accembly plugin added and make executable jar

--- src/main/java/maven_pluginpractice/test.java
+++ src/main/java/maven_pluginpractice/test.java
@@ -1,10 +1,20 @@
 package maven_pluginpractice;
 
+import org.apache.log4j.Logger;
+
 public class test {
+    final static Logger logger = Logger.getLogger(test.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("thuiis is");
+     System.out.println("this is the way to create executable jar");
+     logger.warn("this is warining messages");
+     
+	}
+	public int addition(int x,int y){
+		int z;
+		z=x+y;
+		return z;
 	}
 
 }
